24fd6a33b094cc369694e6c5bd53c571960c7116
Rushijoshi3798 <rushijoshi3798@gmail.com>
2022-09-28 20:47:33 +0530

    signup & create account done

---
+++ day-2/signup.js
@@ -0,0 +1,12 @@
+document.querySelector("#navbar>img").addEventListener("click",imgFun);
+
+function imgFun(){
+    // console.log("hello")
+}
+
+document.querySelector("form").addEventListener("submit",function(event){
+    event.preventDefault();
+    // console.log("hey")
+
+    
+})
