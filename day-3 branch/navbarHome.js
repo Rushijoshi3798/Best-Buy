058693aab44987dc9b52a17356c9b6283a274be2
Rushijoshi3798 <rushijoshi3798@gmail.com>
2022-10-01 13:59:53 +0530

    donr

--- day-3 branch/navbarHome.js
+++ day-3 branch/navbarHome.js
@@ -1,9 +1,21 @@
-// let AddCart = document.querySelector(".AddCart");
 
-// AddCart.addEventListener("click",Myfun);
 
-// function Myfun(){
-//     // console.log("Hello")
-// } //
-//  
+function add_to_cart(pid,pname,pprice){
+    let cart = localstorage.getItem("cart");
+    if (cart == null){
+
+        // No cart yet...
+        let products = [];
+        let product = {
+            productid : pid,
+            productname : pname,
+            productprice : pprice
+        } 
+        products.push(product);
+        localStorage.setItem("cart",JSON.stringify(products));
+    } 
+    else {
+        // cart is already present..
+    }
+}
 
